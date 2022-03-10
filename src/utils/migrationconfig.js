6c005a9a02855a8d8791948e384705f2c6feabaa
brianadams731 <brianadams731@gmail.com>
2022-03-10 13:38:11 -0800

    Added: SSL config for db migragtion config

--- src/utils/migrationconfig.js
+++ src/utils/migrationconfig.js
@@ -1,6 +1,12 @@
 module.exports = {
     type:'postgres',
     url:process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV !== 'development',
+    extra:process.env.NODE_ENV !== 'development'?{
+        ssl:{
+            rejectUnauthorized: false
+        }
+    }:undefined,
     migrations:['src/migrations/*.ts'],
     cli:{
         migrationsDir: "src/migrations"
