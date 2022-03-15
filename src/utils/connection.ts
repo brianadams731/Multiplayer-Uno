6031d283768ccc343da3117b9ec30a863cf38ac4
brianadams731 <brianadams731@gmail.com>
2022-03-15 09:55:34 -0700

    Fix: Added SSL to connection config

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,7 +1,14 @@
 import pgPromise from "pg-promise";
 
+const connectionConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl:process.env.NODE_ENV !== "development"?{
+        rejectUnauthorized : false,
+    }:undefined
+}
+
 const pgp = pgPromise();
-const connection = pgp(process.env.DATABASE_URL as string);
+const connection = pgp(connectionConfig);
 
 
 
