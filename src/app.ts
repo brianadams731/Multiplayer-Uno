1e869fa63ff618ad75ce5e77dec5f52ec453c785
brianadams731 <brianadams731@gmail.com>
2022-04-03 09:57:35 -0700

    Chore: Formated existing code with prettier

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
-import dotenv from "dotenv";
-if(process.env.NODE_ENV === 'development') {
+import dotenv from 'dotenv';
+if (process.env.NODE_ENV === 'development') {
     dotenv.config();
 }
-import express from "express";
+import express from 'express';
 
-import { exampleRoute } from "./routes/exampleRoute";
+import { exampleRoute } from './routes/exampleRoute';
 
 const app = express();
 app.use(express.json());
-app.use("/tests",exampleRoute);
+app.use('/tests', exampleRoute);
 
-app.get("/",(req,res)=>{
-    return res.send("Landing Page");
-})
+app.get('/', (req, res) => {
+    return res.send('Landing Page');
+});
 
-app.listen(process.env.PORT||"8080",()=>{
-    console.log(`Server running at http://localhost:${process.env.PORT||"8080"}/`);
-})
-
+app.listen(process.env.PORT || '8080', () => {
+    console.log(
+        `Server running at http://localhost:${process.env.PORT || '8080'}/`
+    );
+});
--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,15 +1,16 @@
-import pgPromise from "pg-promise";
+import pgPromise from 'pg-promise';
 
 const connectionConfig = {
     connectionString: process.env.DATABASE_URL,
-    ssl:process.env.NODE_ENV !== "development"?{
-        rejectUnauthorized : false,
-    }:undefined
-}
+    ssl:
+        process.env.NODE_ENV !== 'development'
+            ? {
+                  rejectUnauthorized: false,
+              }
+            : undefined,
+};
 
 const pgp = pgPromise();
 const connection = pgp(connectionConfig);
 
-
-
-export {connection};
+export { connection };
