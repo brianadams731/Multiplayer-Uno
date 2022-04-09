b5d4ea79afd33416a086c373815de0fdb7d2ad6c
brianadams731 <brianadams731@gmail.com>
2022-04-09 16:59:23 -0700

    Added: Process orchestration for scss, tsnode and multiple tsconfig files

--- src/routes/viewRoutes.ts
+++ src/routes/viewRoutes.ts
@@ -2,12 +2,16 @@ import express from 'express';
 
 const viewRoutes = express.Router();
 
+viewRoutes.get('/hbtest', (req, res) => {
+    return res.render(`test`, {
+        test: 'From Router',
+    });
+});
 
-
-viewRoutes.get('/hbtest',(req,res)=>{
-    return res.render(`test`,{
-        test:"From Router"
+viewRoutes.get('/play', (req,res) =>{
+    return res.render('uno',{
+        layout: "game.handlebars"
     })
-})
+});
 
 export { viewRoutes };
